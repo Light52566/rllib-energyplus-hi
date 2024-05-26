a913c8a47ffd47fc64a63f2c3312737a9d302642
light52566 <atakorkusuz@gmail.com>
2024-05-26 14:26:51 +0200

    new test for new environment

---
+++ test1.py
@@ -0,0 +1,51 @@
+
+from rleplus.examples.bbright.env import BBrightEnv
+
+from pythermalcomfort.models import pmv, pmv_ppd
+from pythermalcomfort.utilities import v_relative, clo_dynamic
+from pythermalcomfort.utilities import met_typical_tasks
+from pythermalcomfort.utilities import clo_individual_garments
+
+import numpy as np
+
+env = BBrightEnv({"output": "/tmp/tests_output"})
+
+# input variables
+tdb = 27  # dry bulb air temperature, [$^{\circ}$C]
+tr = 25  # mean radiant temperature, [$^{\circ}$C]
+v = 0.3  # average air speed, [m/s]
+rh = 50  # relative humidity, [%]
+activity = "Typing"  # participant's activity description
+garments = ["Sweatpants", "T-shirt"]
+
+met = met_typical_tasks[activity]  # activity met, [met]
+icl = sum(
+    [clo_individual_garments[item] for item in garments]
+)  # calculate total clothing insulation
+
+# calculate the relative air velocity
+vr = v_relative(v=v, met=met)
+# calculate the dynamic clothing insulation
+clo = clo_dynamic(clo=icl, met=met)
+
+obs, _ = env.reset()
+# print(obs)
+pmvs = []
+rewards = []
+# calculate PMV in accordance with the ASHRAE 55 2020
+# results = pmv_ppd(tdb=tdb, tr=tr, vr=vr, rh=rh, met=met, clo=clo, standard="ASHRAE")
+# pmvs.append(results['pmv'])
+'''
+for i in range(2):
+    obs, rew, done, _, _ = env.step(25)
+    # results = pmv_ppd(tdb=obs[1], tr=obs[1], vr=vr, rh=rh, met=met, clo=clo, standard="ASHRAE")
+    # pmvs.append(results['pmv'])
+    # rewards.append(rew)
+    if done:
+        break
+print(obs, rew, done)
+print('indoor tempt:', obs[1])
+print('Mean pmv:', np.array(pmvs).mean())
+print('Mean reward:', np.array(rewards).mean())
+'''
+env.close()
